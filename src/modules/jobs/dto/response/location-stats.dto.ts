030f45b3591109313bc7b9c88e439931c0352e52
qvinh <doanquangvinh69@gmail.com>
2025-08-09 15:18:49 +0700

    update image - vinhdq - 09.08/2025

--- src/modules/jobs/dto/response/location-stats.dto.ts
+++ src/modules/jobs/dto/response/location-stats.dto.ts
@@ -3,25 +3,38 @@ import { ApiProperty } from '@nestjs/swagger';
 export class LocationStatsDto {
   @ApiProperty({
     description: 'Location name',
-    example: 'HaNoi'
+    example: 'HaNoi',
   })
   location: string;
 
   @ApiProperty({
     description: 'Number of jobs in this location',
-    example: 15
+    example: 15,
   })
   jobCount: number;
 
   @ApiProperty({
     description: 'Whether this is a major city',
-    example: true
+    example: true,
   })
   isMajorCity: boolean;
 
-  constructor(location: string, jobCount: number, isMajorCity: boolean) {
+  @ApiProperty({
+    description: 'Image URL representing this location',
+    example:
+      'https://cdn-media.sforum.vn/storage/app/media/wp-content/uploads/2024/01/dia-diem-du-lich-o-ha-noi-thumb.jpg',
+  })
+  image?: string;
+
+  constructor(
+    location: string,
+    jobCount: number,
+    isMajorCity: boolean,
+    image?: string,
+  ) {
     this.location = location;
     this.jobCount = jobCount;
     this.isMajorCity = isMajorCity;
+    this.image = image;
   }
-} 
+}
